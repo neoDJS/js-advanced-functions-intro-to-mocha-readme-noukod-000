8fc3bdb8b04fa5d12a87ba9169166c54dc7ae11f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-15 09:03:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true;
+}
+
+function sayHey(){
+  return 'Hey!';
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(flavor){
+  return `I love ${flavor}`;
+}
+
+function shouting(theStars){
+  return theStars.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
